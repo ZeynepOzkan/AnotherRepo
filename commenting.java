7bb8be29c8cc9f2177153c1d4b0c0135743a8b2d
Zeynep Ozkan <zeynepozkan0320@gmail.com>
2020-09-30 22:55:59 +0200

    edited two awesome files

--- commenting.java
+++ commenting.java
@@ -3,18 +3,19 @@ package day2_printing_comments;
 public class commenting {
 
 	public static void main(String[] args) {
+
+		//write some code here
+
 		
-		//Author:Ozzy
-		//Time:4:30PM
-		
+
 		System.out.println("Hello World");
-		
+
 		/*Today class was about print statements,
 		and we did several tasks for practise.*/
-		
-		
+
+
 		/*
-		 * 
+		 *
 		 */
 	}
 
